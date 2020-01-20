6a476666f12f08f0106c91280bc9933ee3063ebd
LJoel777 <szaknoel@gmail.com>
2020-01-20 12:00:13 +0100

    Added routs

--- server.py
+++ server.py
@@ -4,7 +4,48 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
+@app.route("/list")
+def route_list():
+    return "Hello World!"
+
+
+@app.route("/question/<question_id>")
+def route_question():
+    return "Hello World!"
+
+
+@app.route("/question/add-question")
+def route_question():
+    return "Hello World!"
+
+
+@app.route("/question/<question_id>/new-answer")
+def route_question():
+    return "Hello World!"
+
+
+@app.route("/question/<question_id>/delete")
+def route_question():
+    return "Hello World!"
+
+
+@app.route("/question/<question_id>/edit")
+def route_question():
+    return "Hello World!"
+
+
+@app.route("/answer/<answer_id>delete")
+def route_question():
+    return "Hello World!"
+
+
+@app.route("/question/<question_id>/vote_up")
+def route_question():
+    return "Hello World!"
+
+
+@app.route("/question/<question_id>/vote_down")
+def route_question():
     return "Hello World!"
 
 
