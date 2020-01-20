5ed8c59c0db62384e638f8b101229c3902183cd3
LJoel777 <szaknoel@gmail.com>
2020-01-20 12:34:20 +0100

    Updated server

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -15,39 +15,53 @@ def route_question():
 
 
 @app.route("/question/add-question")
-def route_question():
+def route_add_question():
     return "Hello World!"
 
 
 @app.route("/question/<question_id>/new-answer")
-def route_question():
+def route_new_answer():
     return "Hello World!"
 
 
 @app.route("/question/<question_id>/delete")
-def route_question():
+def route_question_delete():
     return "Hello World!"
 
 
 @app.route("/question/<question_id>/edit")
-def route_question():
+def route_question_edit():
     return "Hello World!"
 
 
 @app.route("/answer/<answer_id>delete")
-def route_question():
+def route_question_delete():
     return "Hello World!"
 
 
 @app.route("/question/<question_id>/vote_up")
-def route_question():
+def route_question_vote_up():
     return "Hello World!"
 
 
 @app.route("/question/<question_id>/vote_down")
-def route_question():
+def route_question_vote_up():
     return "Hello World!"
 
 
-if __name__ == "__main__":
-    app.run()
+@app.route("/question/<answer_id>/vote_up")
+def route_answer_vote_up():
+    return "Hello World!"
+
+
+@app.route("/question/<answer_id>/vote_down")
+def route_answer_vote_up():
+    return "Hello World!"
+
+
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        port=8000,
+        debug=True,
+    )
