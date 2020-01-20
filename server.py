14462ea5d1ee917d6e8da393980658bce41341a8
Adam Elek <elekadam21@gmail.com>
2020-01-20 12:43:58 +0100

    Set up connection.py, data_manager.py, and first templates

--- server.py
+++ server.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/list")
 def route_list():
-    return "Hello World!"
+    questions = data_manager.function()
+    return render_template('list.html', questions=questions)
 
 
 @app.route("/question/<question_id>")
