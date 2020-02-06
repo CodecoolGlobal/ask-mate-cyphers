7ab42bcadcaf7d216312a4580aa9df237b504b96
coryon <illyes.gergely95@gmail.com>
2020-02-06 09:49:08 +0100

    no injection from input (i hope)

--- data_manager.py
+++ data_manager.py
@@ -1,102 +1,129 @@
 import connection
 import datetime
+import util
 
 
 def get_all_questions_with_limit(order_by='id', desc='DESC'):
     query = '''
     SELECT *
     FROM question
-    ORDER BY {} {} LIMIT 5'''.format(order_by, desc)
-    return connection.db_mod_with_return(query=query)
+    ORDER BY %s %s LIMIT 5'''
+    list_of_var = [order_by, desc]
+    return connection.db_mod_list_with_return(query=query, list_of_var=list_of_var)
 
 
 def get_all_questions_without_limit(order_by='id', desc='DESC'):
     query = '''
         SELECT *
         FROM question
-        ORDER BY {} {} '''.format(order_by, desc)
-    return connection.db_mod_with_return(query=query)
+        ORDER BY %s %s'''
+    list_of_var = [order_by, desc]
+    return connection.db_mod_list_with_return(query=query, list_of_var=list_of_var)
 
 
 def get_answers_by_question_id(question_id):
     query = '''
     SELECT *
     FROM answer
-    WHERE question_id = {}'''.format(question_id)
-    return connection.db_mod_with_return(query=query)
+    WHERE question_id = %s'''
+    list_of_var = [question_id]
+    return connection.db_mod_list_with_return(query=query, list_of_var=list_of_var)
 
 
 def get_answer(id_num):
     query = '''
     SELECT *
     FROM answer
-    WHERE id = {}'''.format(id_num)
-    return connection.db_mod_with_return(query=query)
+    WHERE id = %s'''
+    list_of_var = [id_num]
+    return connection.db_mod_list_with_return(query=query, list_of_var=list_of_var)
 
 
 def get_comment(id_type, id_num):
     query = '''
     SELECT *
     FROM comment
-    WHERE {} = {}'''.format(id_type, id_num)
-    return connection.db_mod_with_return(query=query)
+    WHERE %s = %s'''
+    list_of_var = [id_type, id_num]
+    return connection.db_mod_list_with_return(query=query, list_of_var=list_of_var)
 
 
 def get_all_comment():
     query = '''
     SELECT *
     FROM comment'''
-    return connection.db_mod_with_return(query=query)
+    list_of_var = []
+    return connection.db_mod_list_with_return(query=query, list_of_var=list_of_var)
 
 
 def get_question(id_num):
     query = '''
     SELECT *
     FROM question
-    WHERE id={}'''.format(id_num)
-    return connection.db_mod_with_return(query=query)
+    WHERE id=%s'''
+    list_of_var = [id_num]
+    return connection.db_mod_list_with_return(query=query, list_of_var=list_of_var)
 
 
 def vote(table, id_num, num, column):
     query = '''
-    UPDATE {}
-    SET {} = {} + {}
-    WHERE id={}'''.format(table, column, column, num, id_num)
-    connection.db_mod_without_return(query=query)
+    UPDATE %s
+    SET %s = %s + %s
+    WHERE id=%s'''
+    list_of_var = [table, column, column, num, id_num]
+    connection.db_mod_list_without_return(query=query, list_of_var=list_of_var)
 
 
 def add_question(file):
     sub_time = datetime.datetime.now().replace(microsecond=0).isoformat()
     try:
         title, message, image = file
+        query = '''
+            INSERT INTO question(submission_time ,view_number, vote_number, title, message, image)
+            VALUES (%s, 0, 0, %s, %s, %s)'''
+        list_of_var = [sub_time, title, message, image]
     except ValueError:
-        image = None
         title = file[0]
         message = file[1]
-    query = '''
-    INSERT INTO question(submission_time ,view_number, vote_number, title, message, image)
-    VALUES ('{}', 0, 0, '{}', '{}', '{}')'''.format(sub_time, title, message, image)
-    connection.db_mod_without_return(query=query)
+        query = '''
+        INSERT INTO question(submission_time ,view_number, vote_number, title, message)
+        VALUES (%s, 0, 0, %s, %s)'''
+        list_of_var = [sub_time, title, message]
+    connection.db_mod_list_without_return(query=query, list_of_var=list_of_var)
 
 
 def add_answer(file, id_num):
     try:
         message, image = file
+        query = '''
+            INSERT INTO answer(submission_time, vote_number, question_id, message, image)
+            VALUES (current_timestamp, 0, %s, %s, %s)'''
+        list_of_var = [id_num, message, image]
     except ValueError:
-        image = None
         message = file[0]
-    query = '''
-    INSERT INTO answer(submission_time, vote_number, question_id, message, image)
-    VALUES (current_timestamp, 0, {}, '{}', '{}')'''.format(id_num, message, image)
-    connection.db_mod_without_return(query=query)
+        query = '''
+            INSERT INTO answer(submission_time, vote_number, question_id, message)
+            VALUES (current_timestamp, 0, %s, %s)'''
+        list_of_var = [id_num, message]
+    connection.db_mod_list_without_return(query=query, list_of_var=list_of_var)
 
 
-def add_comment(id_type, message, id_num):
+def add_comment_to_question(message, id_num):
     sub_time = datetime.datetime.now().replace(microsecond=0).isoformat()
     query = '''
-    INSERT INTO comment({}, message, submission_time)
-    VALUES ({}, '{}', '{}')'''.format(id_type, id_num, message, sub_time)
-    connection.db_mod_without_return(query=query)
+    INSERT INTO comment(question_id, message, submission_time)
+    VALUES (%s, %s, %s)'''
+    list_of_var = [id_num, message, sub_time]
+    connection.db_mod_list_without_return(query=query, list_of_var=list_of_var)
+
+
+def add_comment_to_answer(message, id_num):
+    sub_time = datetime.datetime.now().replace(microsecond=0).isoformat()
+    query = '''
+    INSERT INTO comment(answer_id, message, submission_time)
+    VALUES (%s, %s, %s)'''
+    list_of_var = [id_num, message, sub_time]
+    connection.db_mod_list_without_return(query=query, list_of_var=list_of_var)
 
 
 def edit_question(file, id_num):
@@ -105,32 +132,35 @@ def edit_question(file, id_num):
         title, message, image = file
         query = '''
             UPDATE question
-            SET title = '{}', message = '{}', image = '{}', submission_time = '{}'
-            WHERE id = {}'''.format(title, message, image, sub_time, id_num)
-        return connection.db_mod_without_return(query=query)
+            SET title = %s, message = %s, image = %s, submission_time = %s
+            WHERE id = %s'''
+        list_of_var = [title, message, image, sub_time, id_num]
     except ValueError:
         title, message = file
         query = '''
             UPDATE question
-            SET title = '{}', message = '{}', submission_time = '{}'
-            WHERE id = {}'''.format(title, message, sub_time, id_num)
-        return connection.db_mod_without_return(query=query)
+            SET title = %s, message = %s, submission_time = %s
+            WHERE id = %s'''
+        list_of_var = title, message, sub_time, id_num
+    connection.db_mod_list_without_return(query=query, list_of_var=list_of_var)
 
 
 def edit_comment(id_num, message):
     sub_time = datetime.datetime.now().replace(microsecond=0).isoformat()
     query = '''
     UPDATE comment
-    SET  message = '{}', submission_time = '{}'
-    WHERE id = {}'''.format(message, sub_time, id_num)
-    return connection.db_mod_without_return(query=query)
+    SET  message = %s, submission_time = %s
+    WHERE id = %s'''
+    list_of_var = [message, sub_time, id_num]
+    connection.db_mod_list_without_return(query=query, list_of_var=list_of_var)
 
 
 def delete_by_id(table, id_type, id_num):
     query = '''
-    DELETE FROM {}
-    WHERE {} = {}'''.format(table, id_type, id_num)
-    return connection.db_mod_without_return(query=query)
+    DELETE FROM %s
+    WHERE %s = %s'''
+    list_of_var = [table, id_type, id_num]
+    connection.db_mod_list_without_return(query=query, list_of_var=list_of_var)
 
 
 def edit_answer(file, id_num):
@@ -139,30 +169,38 @@ def edit_answer(file, id_num):
         message, image = file
         query = '''
             UPDATE answer
-            SET message = '{}', image = '{}', submission_time = '{}'
-            WHERE id = {}'''.format(message, image, sub_time, id_num)
-        return connection.db_mod_without_return(query=query)
+            SET message = %s, image = %s, submission_time = %s
+            WHERE id = %s'''
+        list_of_var = [message, image, sub_time, id_num]
     except ValueError:
         message = file[0]
-    query = '''
-        UPDATE answer
-        SET message = '{}', submission_time = '{}'
-        WHERE id = {}'''.format(message, sub_time, id_num)
-    return connection.db_mod_without_return(query=query)
+        query = '''
+            UPDATE answer
+            SET message = %s, submission_time = %s
+            WHERE id = %s'''
+        list_of_var = [message, sub_time, id_num]
+    connection.db_mod_list_without_return(query=query, list_of_var=list_of_var)
 
 
 def search_question(search):
+    search = util.modify_search(search)
+    search = '%' + search + '%'
     query = '''
     SELECT * FROM question
-    WHERE title LIKE '%{}%' OR message LIKE '%{}%' '''.format(search, search)
-    return connection.db_mod_with_return(query)
+    WHERE title LIKE %s OR message LIKE %s '''
+    list_of_var = [search, search]
+    return connection.db_mod_list_with_return(query=query, list_of_var=list_of_var)
 
 
 def search_answer(search):
-    query = '''
+    search = util.modify_search(search)
+    search = '%' + search + '%'
+    print(search)
+    query = """
     SELECT * FROM answer
-    WHERE message LIKE '%{}%' '''.format(search)
-    return connection.db_mod_with_return(query)
+    WHERE message LIKE %s ESCAPE '\'"""
+    list_of_var = [search]
+    return connection.db_mod_list_with_return(query=query, list_of_var=list_of_var)
 
 
 def get_tags(question_id):
@@ -170,23 +208,26 @@ def get_tags(question_id):
         SELECT tag.name
         FROM question_tag
         INNER JOIN tag ON question_tag.tag_id=tag.id
-        WHERE question_id = {}'''.format(question_id)
-    return connection.db_mod_with_return(query=query)
+        WHERE question_id = %s'''
+    list_of_var = [question_id]
+    return connection.db_mod_list_with_return(query=query, list_of_var=list_of_var)
 
 
 def get_tag_id(tag_name):
     query = '''
         SELECT id
         FROM tag
-        WHERE name = '{}' '''.format(tag_name)
-    return connection.db_mod_with_return(query=query)
+        WHERE name = %s'''
+    list_of_var = [tag_name]
+    return connection.db_mod_list_with_return(query=query, list_of_var=list_of_var)
 
 
 def get_all_tag_names():
     query = '''
         SELECT name
         FROM tag'''
-    return connection.db_mod_with_return(query=query)
+    list_of_var = []
+    return connection.db_mod_list_with_return(query=query, list_of_var=list_of_var)
 
 
 def modify_tags(question_id, new_tags):
@@ -197,17 +238,20 @@ def modify_tags(question_id, new_tags):
             tag_id = get_tag_id(tag)[0]['id']
             query = '''
             DELETE FROM question_tag
-            WHERE question_id = '{}' AND tag_id = '{}' '''.format(question_id, tag_id)
-            connection.db_mod_without_return(query=query)
+            WHERE question_id = %s AND tag_id = %s'''
+            list_of_var = [question_id, tag_id]
+            connection.db_mod_list_without_return(query=query, list_of_var=list_of_var)
     for tag in new_tags:
         if tag not in old_tags:
             if tag not in all_tags:
                 query = '''
                 INSERT INTO tag (name)
-                VALUES ('{}')'''.format(tag)
-                connection.db_mod_without_return(query=query)
+                VALUES (%s)'''
+                list_of_var = [tag]
+                connection.db_mod_list_without_return(query=query, list_of_var=list_of_var)
             tag_id = get_tag_id(tag)[0]['id']
             query = '''
             INSERT INTO question_tag(question_id, tag_id)
-            VALUES ({}, {})'''.format(question_id, tag_id)
-            connection.db_mod_without_return(query=query)
+            VALUES (%s, %s)'''
+            list_of_var = [question_id, tag_id]
+            connection.db_mod_list_without_return(query=query, list_of_var=list_of_var)
--- util.py
+++ util.py
@@ -3,3 +3,17 @@ def get_new_order_dir(order_direction):
         return 'desc'
     else:
         return 'asc'
+
+
+def modify_search(search):
+    length = 0
+    char = ["'", "%"]
+    while length != len(search):
+        print(length, end="")
+        print(len(search))
+        if search[length] in char:
+            search = search[:length] + "\\" + search[length:]
+            length += 2
+        else:
+            length += 1
+    return search
